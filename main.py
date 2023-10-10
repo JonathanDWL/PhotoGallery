e29023707f2497da1c4b107b0c533b65a6751c3c
s10043499 <jonathan.lamping7y@stu.austinisd.org>
2023-10-10 10:34:04 -0500

    Created working photo gallery

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+
 class PhotoGalleryApp(App):
     pass
+
+class Display(Screen):
+    def display(self):
+        return(images[index])
+    def next(self):
+        global index
+        index = (index+1)%len(images)
+        self.ids.image.source = images[index]
+    def prev(self):
+        global index
+        index = (index-1)%len(images)
+        self.ids.image.source = images[index]
+
+images = ["flowah.jpg", "froggy.jpg", "lake.jpg", "man.jpg", "milky.jpg", "tree.jpg"]
+index = 0
+
+PhotoGalleryApp().run()
